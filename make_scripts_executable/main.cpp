0656899966739ea28387b60a413fb0b8ed994f85
Robert Impey <robert-impey@users.noreply.github.com>
2022-08-02 21:52:39 -0600

    MSE: Test file for is script

--- make_scripts_executable/main.cpp
+++ make_scripts_executable/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <fstream>
 
-using namespace std;
+using std::cout; using std::cerr;
+using std::endl; using std::string;
+using std::ifstream;
+
+bool test(string file) {
+    string line;
+
+    ifstream input_file(file);
+    if (!input_file.is_open()) {
+        cerr << "Could not open the file - '"
+             << file << "'" << endl;
+        return false;
+    }
+
+    if (getline(input_file, line)) {
+        input_file.close();
+
+        return line.rfind("#!", 0) == 0;
+    }
+
+    return false;
+}
 
 int main(int argc, char *argv[]) {
     if (argc == 3) {
@@ -10,6 +32,12 @@ int main(int argc, char *argv[]) {
             string file = argv[2];
 
             cout << "Testing that " << file << " is a script." << endl;
+
+            if (test(file)) {
+                cout << "It is!" << endl;
+            } else {
+                cout << "It is not!" << endl;
+            }
 
             return 0;
         }
