c5cdb03f062f9c9cb249899af6794c5f8393759f
Robert Impey <robert-impey@users.noreply.github.com>
2022-09-12 21:36:38 +0100

    MSE: Clear sub name

--- make_scripts_executable/main.cpp
+++ make_scripts_executable/main.cpp
@@ -5,7 +5,33 @@ using std::cout; using std::cerr;
 using std::endl; using std::string;
 using std::ifstream;
 
-bool test(string file) {
+bool test_file_starts_with_shebang(const string& file);
+
+int main(int argc, char *argv[]) {
+    if (argc == 3) {
+        string task = argv[1];
+
+        if (task == "test") {
+            string file = argv[2];
+
+            cout << "Testing if " << file << " starts with a shebang." << endl;
+
+            if (test_file_starts_with_shebang(file)) {
+                cout << "It does!" << endl;
+            } else {
+                cout << "It does not!" << endl;
+            }
+
+            return 0;
+        }
+    }
+
+    cerr << "I don't understand!" << endl;
+
+    return -1;
+}
+
+bool test_file_starts_with_shebang(const string &file) {
     string line;
 
     ifstream input_file(file);
@@ -22,28 +48,4 @@ bool test(string file) {
     }
 
     return false;
-}
-
-int main(int argc, char *argv[]) {
-    if (argc == 3) {
-        string task = argv[1];
-
-        if (task == "test") {
-            string file = argv[2];
-
-            cout << "Testing that " << file << " is a script." << endl;
-
-            if (test(file)) {
-                cout << "It is!" << endl;
-            } else {
-                cout << "It is not!" << endl;
-            }
-
-            return 0;
-        }
-    }
-
-    cerr << "I don't understand!" << endl;
-
-    return -1;
 }
