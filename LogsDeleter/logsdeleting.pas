4a073644040ef6c2687d2ad30f6b974a084ce935
Robert Impey <robert-impey@users.noreply.github.com>
2022-10-12 00:09:53 +0100

    LD: Listing subdirs

--- LogsDeleter/logsdeleting.pas
+++ LogsDeleter/logsdeleting.pas
@@ -8,9 +8,35 @@ procedure DeleteLogs(LogsDir : string);
 
 implementation
 
+uses
+  Classes, FileUtil, SysUtils;
+
 procedure DeleteLogs(LogsDir : string);
+var
+   SearchPath : string;
+   Info : TSearchRec;
+   Count : longint;
 begin
-  Writeln (LogsDir, ' is a directory');
+  Writeln(Format('Searching for subdirectories of %s', [LogsDir]));
+
+  SearchPath := Concat(LogsDir, '/*');
+  DoDirSeparators(SearchPath);
+  Count:=0;
+  If FindFirst (SearchPath,faAnyFile,Info)=0 then
+    begin
+    Repeat
+      With Info do
+        begin
+          If ((Attr and faDirectory) = faDirectory) and (Name<>'.') and (Name<>'..') then
+          begin
+            Writeln(Name);
+            Inc(Count);
+          end;
+        end;
+    Until FindNext(info)<>0;
+    FindClose(Info);
+    end;
+  Writeln ('Finished search. Found ',Count,' matches');
 end;
 
 end.
