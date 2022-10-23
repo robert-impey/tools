d5c89374a291f5c6822bd09925bf6e720055a504
Robert Impey <robert-impey@users.noreply.github.com>
2022-10-23 23:26:23 +0100

    LD: Delete the old log files

--- LogsDeleter/logsdeleting.pas
+++ LogsDeleter/logsdeleting.pas
@@ -5,7 +5,7 @@ unit LogsDeleting;
 interface
 
 uses
-    Classes, FileUtil, SysUtils;
+    Classes, FileUtil, SysUtils, DateUtils;
 
 procedure DeleteLogs(LogsDir : string);
 procedure DeleteLogsFromSubDir(SubDirPath: string);
@@ -14,37 +14,60 @@ implementation
 
 procedure DeleteLogs(LogsDir : string);
 var
-   SearchPath, SubDirsPath : string;
-   Info : TSearchRec;
-   Count : longint;
+  SearchPath, SubDirsPath : string;
+  Info : TSearchRec;
 begin
   Writeln(Format('Searching for subdirectories of %s', [LogsDir]));
 
   SearchPath := Concat(LogsDir, '/*');
   DoDirSeparators(SearchPath);
-  Count:=0;
   If FindFirst (SearchPath,faAnyFile,Info)=0 then
-    begin
+  begin
     Repeat
       With Info do
+      begin
+        If ((Attr and faDirectory) = faDirectory) and (Name<>'.') and (Name<>'..') then
         begin
-          If ((Attr and faDirectory) = faDirectory) and (Name<>'.') and (Name<>'..') then
-          begin
-               SubDirsPath := Concat(LogsDir, '/', Name);
-               DoDirSeparators(SubDirsPath);
-               DeleteLogsFromSubDir(SubDirsPath);
-               Inc(Count);
-          end;
+          SubDirsPath := Concat(LogsDir, '/', Name);
+          DoDirSeparators(SubDirsPath);
+          DeleteLogsFromSubDir(SubDirsPath);
         end;
+      end;
     Until FindNext(Info)<>0;
     FindClose(Info);
-    end;
-  Writeln ('Finished search. Found ',Count,' matches');
+  end;
 end;
 
 procedure DeleteLogsFromSubDir(SubDirPath: string);
+var
+  Threshold, LogFileDate: TDateTime;
+  LogFiles: TStringList;
+  I: integer;
+  FA: LongInt;
 begin
-     Writeln(Format('Searching for log files in %s', [SubDirPath]));
+  Writeln(Format('Searching for log files in %s', [SubDirPath]));
+
+  Threshold := IncMonth(Date,-1);
+
+  LogFiles := FindAllFiles(SubDirPath, '*.log;*.err', false);
+  try
+    Writeln(Format('Found %d Log files', [LogFiles.Count]));
+    for I:=0 to pred(LogFiles.Count) do
+    begin
+      FA:=FileAge(LogFiles[I]);
+      If FA<>-1 then
+      begin
+        LogFileDate:=FileDateToDateTime(FA);
+        if DateOf(LogFileDate) <= DateOf(Threshold) then
+        begin
+          Writeln(Format('%s is old - deleting...', [LogFiles[I]]));
+          DeleteFile(LogFiles[I]);
+        end;
+      end;
+    end;
+  finally
+    LogFiles.Free;
+  end;
 end;
 
 end.
