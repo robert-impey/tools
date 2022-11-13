db8f26aa3db0af1c36d8048fed81584956ff2123
Robert Impey <robert-impey@users.noreply.github.com>
2022-11-13 22:00:32 +0000

    MF: Open the files

--- ManagedFolders/ManagedFolders/ManagedFolders.cpp
+++ ManagedFolders/ManagedFolders/ManagedFolders.cpp
@@ -2,17 +2,20 @@
 //
 
 #include <iostream>
+#include <filesystem>
+#include <fstream>
 
 using namespace std;
 
-void manage_local_scripts(string);
+namespace fs = std::filesystem;
+
+void manage_local_scripts(const fs::path &local_scripts_dir);
 
 int main(int argc, char* argv[])
 {
     if (argc == 2)
     {
-        string local_scripts_dir = argv[1];
-
+        fs::path local_scripts_dir = argv[1];
         manage_local_scripts(local_scripts_dir);
 
         return 0;
@@ -22,7 +25,27 @@ int main(int argc, char* argv[])
     return 1;
 }
 
-void manage_local_scripts(string local_scripts_dir)
+void manage_local_scripts(const fs::path &local_scripts_dir)
 {
+    const auto folders_file_path = local_scripts_dir / "_Common" / "folders.txt";
+
+    ifstream folders_file_stream(folders_file_path);
+
+    if (!folders_file_stream.is_open()) {
+        cerr << "Could not open the folders file - '"
+            << folders_file_path << "'" << endl;
+        return;
+    }
+
+    const auto locations_file_path = local_scripts_dir / "_Common" / "locations.txt";
+
+    ifstream locations_file_stream(locations_file_path);
+
+    if (!locations_file_stream.is_open()) {
+        cerr << "Could not open the locations file - '"
+            << locations_file_path << "'" << endl;
+        return;
+    }
+
 
 }
