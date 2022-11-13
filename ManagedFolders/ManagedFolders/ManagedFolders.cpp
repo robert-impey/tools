d47a23ba2dd731e821fc75ae925c504c2fde3763
Robert Impey <robert-impey@users.noreply.github.com>
2022-11-13 23:02:09 +0000

    MF: Cartesian product of locations and folders

--- ManagedFolders/ManagedFolders/ManagedFolders.cpp
+++ ManagedFolders/ManagedFolders/ManagedFolders.cpp
@@ -27,25 +27,63 @@ int main(int argc, char* argv[])
 
 void manage_local_scripts(const fs::path &local_scripts_dir)
 {
-    const auto folders_file_path = local_scripts_dir / "_Common" / "folders.txt";
-
-    ifstream folders_file_stream(folders_file_path);
-
-    if (!folders_file_stream.is_open()) {
-        cerr << "Could not open the folders file - '"
-            << folders_file_path << "'" << endl;
-        return;
-    }
-
     const auto locations_file_path = local_scripts_dir / "_Common" / "locations.txt";
 
     ifstream locations_file_stream(locations_file_path);
-
     if (!locations_file_stream.is_open()) {
         cerr << "Could not open the locations file - '"
             << locations_file_path << "'" << endl;
         return;
     }
 
+    const auto folders_file_path = local_scripts_dir / "_Common" / "folders.txt";
 
+    ifstream folders_file_stream(folders_file_path);
+    if (!folders_file_stream.is_open()) {
+        cerr << "Could not open the folders file - '"
+            << folders_file_path << "'" << endl;
+        return;
+    }
+
+    vector<string> folders, locations;
+
+    string folders_line, locations_line;
+    while (getline(locations_file_stream, locations_line))
+    {
+        if (locations_line.size())
+        {
+            locations.push_back(locations_line);
+        }
+    }
+    locations_file_stream.close();
+
+    while (getline(folders_file_stream, folders_line))
+    {
+        if (folders_line.size())
+        {
+            folders.push_back(folders_line);
+        }
+    }
+    folders_file_stream.close();
+    
+    auto first = true;
+    for (auto &location : locations)
+    {
+        if (first)
+            first = false;
+        else
+            cout << endl;
+
+        const fs::path location_path = location;
+
+        for (auto& folder : folders)
+        {
+            const fs::path located_folder_path = location_path / folder;
+
+            if (fs::exists(located_folder_path))
+            {
+                cout << located_folder_path << endl;
+            }
+        }
+    }
 }
