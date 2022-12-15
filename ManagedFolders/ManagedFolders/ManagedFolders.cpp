39f52c5955f5f5c0e3a89c2a52cb42da3b5b2180
Robert Impey <robert-impey@users.noreply.github.com>
2022-12-15 22:57:20 +0000

    MF: Generate folders

--- ManagedFolders/ManagedFolders/ManagedFolders.cpp
+++ ManagedFolders/ManagedFolders/ManagedFolders.cpp
@@ -5,12 +5,15 @@
 #include <filesystem>
 #include <fstream>
 #include <vector>
+#include <regex>
 
 using namespace std;
 
 namespace fs = std::filesystem;
 
 vector<string> read_all_non_empty_lines(const fs::path& path);
+fs::path find_autogen_path();
+string clean_path(fs::path);
 
 class FolderManager
 {
@@ -24,7 +27,18 @@ public:
 
 		locations = read_all_non_empty_lines(locations_file_path);
 		folders = read_all_non_empty_lines(folders_file_path);
+
+		for (auto& location : locations)
+		{
+			const fs::path location_path{ location };
+
+			if (fs::exists(location_path))
+			{
+				location_paths.push_back(location_path);
+			}
+		}
 	}
+
 	void list()
 	{
 		auto first{ true };
@@ -48,6 +62,7 @@ public:
 			}
 		}
 	}
+
 	void list_pairs()
 	{
 		auto pairs = find_pairs();
@@ -57,9 +72,26 @@ public:
 			cout << a_pair.first << " <-> " << a_pair.second << endl;
 		}
 	}
+
+	void generate_synch_scripts()
+	{
+		auto autogen_path = find_autogen_path();
+
+		auto synch_autogen_path{ autogen_path / "synch" };
+
+		if (!fs::exists(synch_autogen_path))
+		{
+			fs::create_directory(synch_autogen_path);
+		}
+
+		generate_synch_location_pair_folders(synch_autogen_path);
+	}
+
 private:
 	fs::path local_scripts_dir, locations_file_path, folders_file_path;
 	vector<string> locations, folders;
+	vector<fs::path> location_paths;
+
 	vector<pair<fs::path, fs::path>> find_pairs()
 	{
 		vector<pair<fs::path, fs::path>> pairs;
@@ -82,7 +114,7 @@ private:
 					if (fs::exists(located_folder_path1) && fs::exists(located_folder_path2))
 					{
 						pair<fs::path, fs::path> a_pair{ located_folder_path1 , located_folder_path2 };
-						pair<fs::path, fs::path> reversed_pair { located_folder_path2 , located_folder_path1 };
+						pair<fs::path, fs::path> reversed_pair{ located_folder_path2 , located_folder_path1 };
 
 						if (std::find(pairs.begin(), pairs.end(), reversed_pair) != pairs.end())
 						{
@@ -97,8 +129,36 @@ private:
 
 		return pairs;
 	}
-};
 
+	void generate_synch_location_pair_folders(fs::path synch_autogen_path)
+	{
+		for (auto& location_path1 : location_paths)
+		{
+			const string clean_path1 = clean_path(location_path1);
+
+			const fs::path sub_path1{ synch_autogen_path / clean_path1 };
+
+			if (!fs::exists(sub_path1))
+			{
+				fs::create_directory(sub_path1);
+			}
+
+			for (auto& location_path2 : location_paths)
+			{
+				if (location_path1 == location_path2) continue;
+
+				const string clean_path2 = clean_path(location_path2);
+
+				const fs::path sub_path2{ sub_path1 / clean_path2 };
+
+				if (!fs::exists(sub_path2))
+				{
+					fs::create_directory(sub_path2);
+				}
+			}
+		}
+	}
+};
 
 int main(int argc, char* argv[])
 {
@@ -129,6 +189,13 @@ int main(int argc, char* argv[])
 
 			return 0;
 		}
+
+		if (task == "generate_synch_scripts")
+		{
+			folder_manager.generate_synch_scripts();
+
+			return 0;
+		}
 	}
 
 	cerr << "Please tell me what to do!" << endl;
@@ -154,4 +221,34 @@ vector<string> read_all_non_empty_lines(const fs::path& path)
 	file_stream.close();
 
 	return lines;
+}
+
+fs::path find_autogen_path()
+{
+	auto userprofile_env{ getenv("USERPROFILE") };
+
+	if (userprofile_env == NULL)
+	{
+		throw exception("USERPROFILE env var not set!");
+	}
+
+	fs::path userprofile_path{ userprofile_env };
+	fs::path autogen_path{ userprofile_path / "autogen" };
+
+	if (!fs::exists(autogen_path))
+	{
+		fs::create_directory(autogen_path);
+	}
+
+	return autogen_path;
+}
+
+string clean_path(fs::path path)
+{
+	string path_str{ path.string()};
+
+	std::regex illegals{ "[:\\\\/]+" };
+	string replacement{ "_" };
+
+	return std::regex_replace(path_str, illegals, replacement);
 }
