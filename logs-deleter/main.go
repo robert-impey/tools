1127ce4b3ea29316fa0bd0bf5f15d2be9b9696c7
Robert Impey <robert-impey@users.noreply.github.com>
2022-12-20 00:34:43 +0000

    LD: List the contents of the logs dir

--- logs-deleter/main.go
+++ logs-deleter/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/user"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Println("Logs deleter")
+	var err = sweepLogsDir()
+	if err != nil {
+		fmt.Fprint(os.Stderr, err.Error())
+	} else {
+		fmt.Println("Success")
+	}
+}
+
+func sweepLogsDir() error {
+	user, err := user.Current()
+	if err != nil {
+		return err
+	}
+	var logsDir = filepath.Join(user.HomeDir, "logs")
+
+	if _, err := os.Stat(logsDir); errors.Is(err, os.ErrNotExist) {
+		err := os.MkdirAll(logsDir, os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+
+	subDirs, err := filepath.Glob(filepath.Join(logsDir, "*"))
+	if err != nil {
+		return err
+	}
+
+	for _, subDir := range subDirs {
+		subStat, err := os.Stat(subDir)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("%v - %v", subStat.Name(), subStat.ModTime())
+	}
+
+	return nil
 }
