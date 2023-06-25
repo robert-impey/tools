90b66cca7dc3c398ff9b364d222b5b723e70f074
Robert Impey <robert-impey@users.noreply.github.com>
2023-06-25 22:30:29 +0100

    CS: Dev version flag

--- cron-setter/cmd/print.go
+++ cron-setter/cmd/print.go
@@ -21,44 +21,55 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		print()
+		dev, _ := cmd.Flags().GetBool("dev")
+		print(dev)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(printCmd)
+	printCmd.Flags().BoolP("dev", "d", false, "add dev versions to cron")
 }
 
-func print() {
-	printStayDeleted()
-	printResetPerms()
-	printLogsDeleter()
+func print(dev bool) {
+	printStayDeleted(dev)
+	printResetPerms(dev)
+	printLogsDeleter(dev)
 	printSynch()
 }
 
-func printStayDeleted() {
+func printStayDeleted(dev bool) {
+	buildType := getBuildType(dev)
 	fmt.Println("# Stay Deleted")
 	for i := 0; i < 11; i++ {
 		stayDeletedMinutes := rand.Int31n(60)
-		fmt.Printf("%d %d * * * /home/robert/executables/Linux/prod/x64/run-stay-deleted sweepNightly\n", stayDeletedMinutes, i)
+		fmt.Printf("%d %d * * * /home/robert/executables/Linux/%s/x64/run-stay-deleted sweepNightly\n", stayDeletedMinutes, i, buildType)
 	}
 	for i := 18; i < 23; i++ {
 		stayDeletedMinutes := rand.Int31n(60)
-		fmt.Printf("%d %d * * * /home/robert/executables/Linux/prod/x64/run-stay-deleted sweepNightly\n", stayDeletedMinutes, i)
+		fmt.Printf("%d %d * * * /home/robert/executables/Linux/%s/x64/run-stay-deleted sweepNightly\n", stayDeletedMinutes, i, buildType)
 	}
 	fmt.Println()
 }
 
-func printResetPerms() {
+func printResetPerms(dev bool) {
 	resetPermsMinutes := rand.Int31n(60)
 
-	fmt.Printf("%d 11 * * * /usr/bin/zsh /home/robert/local-scripts/_Common/reset-perms/reset-perms.sh\n", resetPermsMinutes)
+	flag := ""
+
+	if dev {
+		flag = " --dev"
+	}
+
+	fmt.Printf("%d 11 * * * /usr/bin/zsh /home/robert/local-scripts/_Common/reset-perms/reset-perms.sh%s\n", resetPermsMinutes, flag)
 }
 
-func printLogsDeleter() {
+func printLogsDeleter(dev bool) {
 	logsDeleterMinutes := rand.Int31n(60)
 
-	fmt.Printf("%d 12 * * * /home/robert/executables/Linux/prod/x64/logs-deleter sweepAll\n", logsDeleterMinutes)
+	buildType := getBuildType(dev)
+
+	fmt.Printf("%d 12 * * * /home/robert/executables/Linux/%s/x64/logs-deleter sweepAll\n", logsDeleterMinutes, buildType)
 }
 
 func printSynch() {
@@ -66,4 +77,11 @@ func printSynch() {
 	synchHours := rand.Int31n(4) + 13
 
 	fmt.Printf("%d %d * * * /usr/bin/zsh /home/robert/local-scripts/_Common/synch/run-nightly.sh\n", synchMinutes, synchHours)
+}
+
+func getBuildType(dev bool) string {
+	if dev {
+		return "dev"
+	}
+	return "prod"
 }
