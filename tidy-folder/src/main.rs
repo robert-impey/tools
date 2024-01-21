77ec66eefc4baca50c6b42428e65f0decfe5d144
Robert Impey <robert-impey@users.noreply.github.com>
2024-01-21 16:56:52 +0000

    TF: Getting the files in their folders

--- tidy-folder/src/main.rs
+++ tidy-folder/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use clap::{Parser};
 use walkdir::WalkDir;
 
@@ -20,15 +21,26 @@ fn main() {
     if let Some(name) = cli.directory.as_deref() {
         println!("Value for directory: {name}");
 
+        let mut dirs_and_files: HashMap<String, Vec<String>>  = HashMap::new();
+
         for entry in WalkDir::new(name).into_iter().filter_map(|e| e.ok()) {
             let path = entry.path();
+            let path_str = path.to_str().unwrap().to_string();
 
-            if path.is_dir() {
-                println!("Directory {}", path.display())
-            } else {
-                println!("File {}", path.display());
+            if path.is_file() {
                 let parent = path.parent().unwrap();
-                println!("Parent {}", parent.display());
+                let parent_str = parent.to_str().unwrap().to_string();
+
+                dirs_and_files.entry(parent_str)
+                    .or_insert_with(Vec::new)
+                    .push(path_str);
+            }
+        }
+
+        for (dir, files) in dirs_and_files {
+            println!("Dir: {}", dir);
+            for file in files {
+                println!("\tFile: {}", file);
             }
         }
     }
