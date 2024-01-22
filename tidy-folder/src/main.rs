843d6bbbd1e99a64b5cb7ef9f4c53b1b5033d6d9
Robert Impey <robert-impey@users.noreply.github.com>
2024-01-22 22:11:50 +0000

    TF: Printing out the other paths

--- tidy-folder/src/main.rs
+++ tidy-folder/src/main.rs
@@ -21,7 +21,7 @@ fn main() {
     if let Some(name) = cli.directory.as_deref() {
         println!("Value for directory: {name}");
 
-        let mut dirs_and_files: HashMap<String, Vec<DirEntry>>  = HashMap::new();
+        let mut dirs_and_files: HashMap<String, Vec<DirEntry>> = HashMap::new();
 
         for entry in WalkDir::new(name).into_iter().filter_map(|e| e.ok()) {
             let path = entry.path();
@@ -38,18 +38,43 @@ fn main() {
 
         for (dir, files) in dirs_and_files {
             println!("Dir: {}", dir);
-            for file in files {
+            for file in files.iter().cloned() {
                 let path = file.path();
                 println!("\tFile: {}", path.display());
 
                 match path.file_stem() {
-                    Some(file_stem) => println!("\t\tFile stem: {:?}", file_stem),
-                    None => println!("\t\tNo file stem")
-                }
+                    Some(file_stem) =>
+                        {
+                            println!("\t\tFile stem: {:?}", file_stem);
 
-                match path.extension() {
-                    Some(extension) => println!("\t\tExtension: {:?}", extension),
-                    None => println!("\t\tNo extension")
+                            match path.extension() {
+                                Some(extension) => {
+                                    println!("\t\tExtension: {:?}", extension);
+
+                                    for other_file in files.iter().cloned() {
+                                        let other_path = other_file.path();
+                                        println!("\tOther File: {}", other_path.display());
+
+                                        match other_path.file_stem() {
+                                            Some(other_file_stem) =>
+                                                {
+                                                    println!("\t\t\tOther File stem: {:?}", other_file_stem);
+
+                                                    match other_path.extension() {
+                                                        Some(other_extension) => {
+                                                            println!("\t\t\tOther Extension: {:?}", other_extension);
+                                                        },
+                                                        None => println!("\t\tNo other extension")
+                                                    }
+                                                },
+                                            None => println!("\t\tNo other file stem")
+                                        }
+                                    }
+                                },
+                                None => println!("\t\tNo extension")
+                            }
+                        },
+                    None => println!("\t\tNo file stem")
                 }
             }
         }
