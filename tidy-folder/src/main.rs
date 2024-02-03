5dacd1039caf25a260617da8c8695140ba221198
Robert Impey <robert-impey@users.noreply.github.com>
2024-02-03 19:36:24 +0000

    TF: Rm file type option

--- tidy-folder/src/main.rs
+++ tidy-folder/src/main.rs
@@ -5,18 +5,11 @@ use walkdir::{DirEntry, WalkDir};
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Optional type of file to search for
-    file_type: Option<String>,
-
     directory: Option<String>,
 }
 
 fn main() {
     let cli = Cli::parse();
-
-    if let Some(name) = cli.file_type.as_deref() {
-        println!("Value for file_type: {name}");
-    }
 
     if let Some(name) = cli.directory.as_deref() {
         println!("Value for directory: {name}");
