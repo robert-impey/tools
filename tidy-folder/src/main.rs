1174f51cd84bbc26e9909c62fcd42028515b62a3
Robert Impey <robert-impey@users.noreply.github.com>
2024-02-03 20:49:20 +0000

    TF: Comment out intermediate print statements

--- tidy-folder/src/main.rs
+++ tidy-folder/src/main.rs
@@ -30,70 +30,70 @@ fn main() {
         }
 
         let mut matching_stems: Vec<DirEntry> = Vec::new();
-        for (dir, files) in dirs_and_files {
-            println!("Dir: {}", dir);
+        for (_dir, files) in dirs_and_files {
+            //println!("Dir: {}", dir);
             for file in files.iter().cloned() {
                 let path = file.path();
-                println!("\tFile: {}", path.display());
+                //println!("\tFile: {}", path.display());
 
                 match path.file_stem() {
                     Some(file_stem) =>
                         {
                             let file_stem_str = file_stem.to_str().unwrap();
 
-                            println!("\t\tFile stem: {}", file_stem_str);
+                            //println!("\t\tFile stem: {}", file_stem_str);
 
                             match path.extension() {
                                 Some(extension) => {
                                     let extension_string = extension.to_str().unwrap();
 
-                                    println!("\t\tExtension: {}", extension_string);
+                                    //println!("\t\tExtension: {}", extension_string);
 
                                     for other_file in files.iter().cloned() {
                                         let other_path = other_file.path();
-                                        println!("\tOther File: {}", other_path.display());
+                                        //println!("\tOther File: {}", other_path.display());
 
                                         match other_path.file_stem() {
                                             Some(other_file_stem) =>
                                                 {
                                                     let other_file_stem_string = other_file_stem.to_str().unwrap();
 
-                                                    println!("\t\t\tOther File stem: {}", other_file_stem_string);
+                                                    //println!("\t\t\tOther File stem: {}", other_file_stem_string);
 
                                                     match other_path.extension() {
                                                         Some(other_extension) => {
                                                             let other_extension_str = other_extension.to_str().unwrap();
 
-                                                            println!("\t\t\tOther Extension: {}", other_extension_str);
+                                                            //println!("\t\t\tOther Extension: {}", other_extension_str);
 
                                                             if extension_string == other_extension_str {
-                                                                println!("\t\t\t\tMatching extension");
+                                                                //println!("\t\t\t\tMatching extension");
 
                                                                 if other_file_stem_string == file_stem_str {
-                                                                    println!("\t\t\t\t{} is {}. Skipping...", other_file_stem_string, file_stem_str);
+                                                                    //println!("\t\t\t\t{} is {}. Skipping...", other_file_stem_string, file_stem_str);
                                                                 } else {
                                                                     if other_file_stem_string.starts_with(file_stem_str) {
-                                                                        println!("\t\t\t\tFile stem match!");
+                                                                        //println!("\t\t\t\tFile stem match!");
                                                                         matching_stems.push(other_file);
                                                                     } else {
-                                                                        println!("\t\t\t\t{} does not start with {}", other_file_stem_string, file_stem_str);
+                                                                        //println!("\t\t\t\t{} does not start with {}", other_file_stem_string, file_stem_str);
                                                                     }
                                                                 }
                                                             } else {
-                                                                println!("\t\t\t\t{} does not equal {}", extension_string, other_extension_str);
+                                                                //println!("\t\t\t\t{} does not equal {}", extension_string, other_extension_str);
                                                             }
                                                         }
-                                                        None => println!("\t\tNo other extension")
+                                                        None => ()//println!("\t\tNo other extension")
                                                     }
                                                 }
-                                            None => println!("\t\tNo other file stem")
+                                            None => () // println!("\t\tNo other file stem")
                                         }
                                     }
                                 }
-                                None => println!("\t\tNo extension")
+                                None => () //println!("\t\tNo extension")
                             }
                         }
-                    None => println!("\t\tNo file stem")
+                    None => () //println!("\t\tNo file stem")
                 }
             }
         }
