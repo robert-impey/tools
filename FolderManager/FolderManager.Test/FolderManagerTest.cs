b7e501009751186df03a23636085c10703dc0590
Robert Impey <robert-impey@users.noreply.github.com>
2024-03-07 05:49:47 +0000

    FM: Added test

---
+++ FolderManager/FolderManager.Test/FolderManagerTest.cs
@@ -0,0 +1,15 @@
+using FluentAssertions;
+
+namespace FolderManager.Test
+{
+    public class FolderManagerTest
+    {
+        [Fact]
+        public void CanGetFolderManager()
+        {
+            var folderManager = FolderManager.GetFolderManager();
+
+            folderManager.Should().NotBeNull();
+        }
+    }
+}
