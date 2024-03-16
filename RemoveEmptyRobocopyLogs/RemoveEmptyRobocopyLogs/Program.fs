6ee04e25ea3c8493e751a9073d2260290e1174a9
Robert Impey <robert-impey@users.noreply.github.com>
2024-03-16 00:38:30 +0000

    RERL: Print if the synch logs dir exists

--- RemoveEmptyRobocopyLogs/RemoveEmptyRobocopyLogs/Program.fs
+++ RemoveEmptyRobocopyLogs/RemoveEmptyRobocopyLogs/Program.fs
@@ -1,2 +1,18 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System.IO
+
 printfn "Looking for Robocopy Log Files"
+
+let logsDirectory =
+    Path.Combine(System.Environment.GetEnvironmentVariable("HOME"), "logs", "synch")
+
+printfn "Synch logs directory: %s" logsDirectory
+
+let synchLogsDirMessage =
+    if Directory.Exists(logsDirectory) then
+        $"synch logs directory %s{logsDirectory} exists"
+    else
+        $"Synch logs directory %s{logsDirectory} does not exist"
+
+printfn $"%s{synchLogsDirMessage}"
