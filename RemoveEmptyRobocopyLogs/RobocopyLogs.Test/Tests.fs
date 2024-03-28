56a73ecfa42b4087d8daaf53cea1d165a0fb91d0
Robert Impey <robert-impey@users.noreply.github.com>
2024-03-28 23:23:56 +0000

    RERL: Failing test to check GH

--- RemoveEmptyRobocopyLogs/RobocopyLogs.Test/Tests.fs
+++ RemoveEmptyRobocopyLogs/RobocopyLogs.Test/Tests.fs
@@ -8,7 +8,11 @@ let ``No files Copied`` () =
     let line = "    Files :      5117         0      5117         0         0         0"
     Assert.False(isFilesCopiedLine line)
 
+[<Fact>]
 let ``Files Copied`` () =
     let line = "    Files :      5117         123      5117         0         0         0"
     Assert.True(isFilesCopiedLine line)
-    
+
+[<Fact>]
+let ``Always fails`` () =
+    Assert.True(false)
