bd49db7e8cce6e23b6d591b2d185095067f50ebc
Robert Impey <robert-impey@users.noreply.github.com>
2024-03-28 21:00:01 +0000

    RERL: Moved out match function

--- RemoveEmptyRobocopyLogs/RemoveEmptyRobocopyLogs/RobocopyLogs.fs
+++ RemoveEmptyRobocopyLogs/RemoveEmptyRobocopyLogs/RobocopyLogs.fs
@@ -7,15 +7,18 @@ let lineRegex = new Regex("\s*Files :\s+\d+\s+(\d+)\s+")
 
 //let line = "    Files :      5117         0      5117         0         0         0"
 //let line = "    Files :      5117         123      5117         0         0         0"
+
+let isFilesCopiedLine (line: string) =
+    let matches = lineRegex.Matches(line)
+    
+    matches.Count > 0 && "0" <> matches[0].Groups[1].Value 
+
 let fileHasCopies (fileName: string) =
     let mutable foundCopiesLine = false
 
     for line in File.ReadAllLines fileName do
         if not foundCopiesLine then
-            let matches = lineRegex.Matches(line)
-
-            if matches.Count > 0 then
-                if "0" <> matches[0].Groups[1].Value then
-                    foundCopiesLine <- true
+            if isFilesCopiedLine line then
+                foundCopiesLine <- true
 
     foundCopiesLine
