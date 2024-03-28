9c4b74110a4c39c8f8990e0d0f8821ff2b3f1ecb
Robert Impey <robert-impey@users.noreply.github.com>
2024-03-28 21:04:58 +0000

    RERL: Use exists

--- RemoveEmptyRobocopyLogs/RemoveEmptyRobocopyLogs/RobocopyLogs.fs
+++ RemoveEmptyRobocopyLogs/RemoveEmptyRobocopyLogs/RobocopyLogs.fs
@@ -7,18 +7,10 @@ let lineRegex = new Regex("\s*Files :\s+\d+\s+(\d+)\s+")
 
 //let line = "    Files :      5117         0      5117         0         0         0"
 //let line = "    Files :      5117         123      5117         0         0         0"
-
 let isFilesCopiedLine (line: string) =
     let matches = lineRegex.Matches(line)
     
     matches.Count > 0 && "0" <> matches[0].Groups[1].Value 
 
 let fileHasCopies (fileName: string) =
-    let mutable foundCopiesLine = false
-
-    for line in File.ReadAllLines fileName do
-        if not foundCopiesLine then
-            if isFilesCopiedLine line then
-                foundCopiesLine <- true
-
-    foundCopiesLine
+    File.ReadAllLines fileName |> Array.exists isFilesCopiedLine
