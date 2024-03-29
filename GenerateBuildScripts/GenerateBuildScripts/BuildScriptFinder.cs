07f218d4e3f28dcd62c34d46c60db835f0b93b36
Robert Impey <robert-impey@users.noreply.github.com>
2024-03-29 19:50:24 +0000

    GBS: Rename the program

---
+++ GenerateBuildScripts/GenerateBuildScripts/BuildScriptFinder.cs
@@ -0,0 +1,43 @@
+﻿namespace RunBuildScripts;
+
+internal class BuildScriptFinder
+{
+    private readonly FolderManager.FolderManager _folderManager;
+
+    private const string buildScript = "Build.ps1";
+
+    public BuildScriptFinder(FolderManager.FolderManager folderManager)
+    {
+        _folderManager = folderManager;
+    }
+
+    public string? GetBuildScriptToCopy()
+    {
+        var localScriptsFolder = _folderManager.GetLocalScriptsFolder();
+
+        var machineLocalScriptsFolder = Path.Join(localScriptsFolder, Environment.MachineName);
+
+        var buildScriptToCopy = Path.Join(machineLocalScriptsFolder, Environment.UserName, buildScript);
+
+        if (File.Exists(buildScriptToCopy))
+        {
+            return buildScriptToCopy;
+        }
+
+        buildScriptToCopy = Path.Join(machineLocalScriptsFolder, buildScript);
+
+        if (File.Exists(buildScriptToCopy))
+        {
+            return buildScriptToCopy;
+        }
+
+        return null;
+    }
+
+    public string GetBuildScriptDestination()
+    {
+        var autogenFolder = _folderManager.GetAutogenFolder();
+
+        return Path.Join(autogenFolder, buildScript);
+    }
+}
---
+++ GenerateBuildScripts/GenerateBuildScripts/Program.cs
@@ -0,0 +1,20 @@
+﻿using RunBuildScripts;
+
+var folderManager = FolderManager.FolderManager.GetFolderManager();
+
+var buildScriptFinder = new BuildScriptFinder(folderManager);
+
+var buildScriptToCopy = buildScriptFinder.GetBuildScriptToCopy();
+
+if (string.IsNullOrEmpty(buildScriptToCopy))
+{
+    Console.WriteLine("No build script found. Quitting...");
+}
+else
+{
+    Console.WriteLine($"Found {buildScriptToCopy}");
+
+    var destination = buildScriptFinder.GetBuildScriptDestination();
+
+    File.Copy(buildScriptToCopy, destination, overwrite: true);
+}
