d435f5fc873e778df0292c0d9ae1af362fbbf0e6
Robert Impey <robert-impey@users.noreply.github.com>
2024-06-26 11:10:52 +0100

    MF: Put method for finding the local scripts dir in lib

--- managed-folders/mflib/mflib.go
+++ managed-folders/mflib/mflib.go
@@ -22,3 +22,41 @@ func GetManagedFoldersFile() (string, error) {
 
 	return filepath.Abs(managedFoldersFile)
 }
+
+func GetLocalScriptsDirectory() (string, error) {
+	if dir, ok := os.LookupEnv("LOCAL_SCRIPTS"); ok {
+		dirStat, err := os.Stat(dir)
+		if err != nil {
+			return "", err
+		}
+		if !dirStat.IsDir() {
+			return "", err
+		}
+		absDir, err := filepath.Abs(dir)
+		if err != nil {
+			return "", err
+		}
+
+		return absDir, nil
+	}
+
+	currentUser, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	localScriptsDir := filepath.Join(currentUser.HomeDir, "local-scripts")
+	localScriptsDirStat, err := os.Stat(localScriptsDir)
+	if err != nil {
+		return "", err
+	}
+
+	if !localScriptsDirStat.IsDir() {
+		return "", err
+	}
+	absLocalScriptsDir, err := filepath.Abs(localScriptsDir)
+	if err != nil {
+		return "", err
+	}
+
+	return absLocalScriptsDir, nil
+}
--- run-stay-deleted/cmd/sweepNightly.go
+++ run-stay-deleted/cmd/sweepNightly.go
@@ -45,7 +45,7 @@ func init() {
 }
 
 func sweepNightly(find bool) error {
-	localScriptsDirectory, err := getLocalScriptsDirectory()
+	localScriptsDirectory, err := mflib.GetLocalScriptsDirectory()
 	if err != nil {
 		return err
 	}
@@ -137,42 +137,4 @@ func sweepNightly(find bool) error {
 	err = sdlib.SweepFrom(nightlyFile, 12, outLogFile, errLogFile, false)
 
 	return err
-}
-
-func getLocalScriptsDirectory() (string, error) {
-	if dir, ok := os.LookupEnv("LOCAL_SCRIPTS"); ok {
-		dirStat, err := os.Stat(dir)
-		if err != nil {
-			return "", err
-		}
-		if !dirStat.IsDir() {
-			return "", err
-		}
-		absDir, err := filepath.Abs(dir)
-		if err != nil {
-			return "", err
-		}
-
-		return absDir, nil
-	}
-
-	currentUser, err := user.Current()
-	if err != nil {
-		return "", err
-	}
-	localScriptsDir := filepath.Join(currentUser.HomeDir, "local-scripts")
-	localScriptsDirStat, err := os.Stat(localScriptsDir)
-	if err != nil {
-		return "", err
-	}
-
-	if !localScriptsDirStat.IsDir() {
-		return "", err
-	}
-	absLocalScriptsDir, err := filepath.Abs(localScriptsDir)
-	if err != nil {
-		return "", err
-	}
-
-	return absLocalScriptsDir, nil
 }
