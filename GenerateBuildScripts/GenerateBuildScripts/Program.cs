78ee36446df6cad5b71e19b720bf0f776ae2382d
Robert Impey <robert-impey@users.noreply.github.com>
2024-08-31 17:43:24 +0100

    GBS: Using NLog

--- GenerateBuildScripts/GenerateBuildScripts/Program.cs
+++ GenerateBuildScripts/GenerateBuildScripts/Program.cs
@@ -1,25 +1,44 @@
 ﻿using GenerateBuildScripts;
+using NLog;
 
-var folderManager = FolderManager.FolderManager.GetFolderManager();
-
-var buildScriptFinder = new BuildScriptFinder(folderManager);
-
-var buildScriptToCopy = buildScriptFinder.GetBuildScriptToCopy();
-
-var destination = buildScriptFinder.GetBuildScriptDestination();
-
-if (File.Exists(destination))
+internal class Program
 {
-    File.Delete(destination);
-}
+    private static void Main(string[] args)
+    {
+        var folderManager = FolderManager.FolderManager.GetFolderManager();
 
-if (string.IsNullOrEmpty(buildScriptToCopy))
-{
-    Console.WriteLine("No build script found. Quitting...");
-}
-else
-{
-    Console.WriteLine($"Found {buildScriptToCopy}");
+        var buildScriptFinder = new BuildScriptFinder(folderManager);
 
-    File.Copy(buildScriptToCopy, destination);
+        var buildScriptToCopy = buildScriptFinder.GetBuildScriptToCopy();
+
+        var destination = buildScriptFinder.GetBuildScriptDestination();
+
+        LogManager.Setup().LoadConfiguration(builder =>
+        {
+            var synchLogsDirectory = Path.Combine(folderManager.GetLogsFolder(), "build");
+            var timeString = DateTime.UtcNow.ToString("yyyy-MM-dd_HH-mm-ss");
+            var fileName = $"{timeString}-GenerateBuildScripts.log";
+            var logFile = Path.Combine(synchLogsDirectory, fileName);
+            builder.ForLogger().WriteToFile(fileName: logFile);
+        });
+
+        var logger = LogManager.GetCurrentClassLogger();
+
+        if (File.Exists(destination))
+        {
+            logger.Info($"Deleting {destination}");
+            File.Delete(destination);
+        }
+
+        if (string.IsNullOrEmpty(buildScriptToCopy))
+        {
+            logger.Info("No build script found. Quitting...");
+        }
+        else
+        {
+            logger.Info($"Found {buildScriptToCopy}");
+
+            File.Copy(buildScriptToCopy, destination);
+        }
+    }
 }
