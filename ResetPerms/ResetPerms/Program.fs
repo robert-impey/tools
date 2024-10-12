6928f53a72f2e2b0343b2d856c4d9bee74f20f14
Robert Impey <robert-impey@users.noreply.github.com>
2024-10-12 18:08:09 +0100

    RP: Find all files

--- ResetPerms/ResetPerms/Program.fs
+++ ResetPerms/ResetPerms/Program.fs
@@ -1,4 +1,12 @@
 ﻿open System.CommandLine
+open FolderManager
+open Microsoft.Extensions.FileSystemGlobbing
+
+let findFilesWithShebang (scriptsDir: string) =
+    let matcher = Matcher()
+    matcher.AddIncludePatterns(seq { "*"})
+    let matchingFiles = matcher.GetResultsInFullPath(scriptsDir)
+    matchingFiles
 
 [<EntryPoint>]
 let main (args) =
@@ -6,12 +14,34 @@ let main (args) =
         Option<bool>("--dry-run", (fun () -> false))
 
     let rootCommand =
-        RootCommand("Reset permissions for files that failed synch")
+        RootCommand("Reset permissions for files that have a shebang")
 
     rootCommand.AddOption(dryRunOption)
-
+    
     let handler (dryRun: bool) =
-        printfn $"%b{dryRun}"
+        let logger =
+            if dryRun then
+                let config = NLog.Config.LoggingConfiguration ()
+
+                let logconsole = new NLog.Targets.ConsoleTarget("logconsole")
+
+                config.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, logconsole)
+
+                NLog.LogManager.Configuration = config |> ignore
+                
+                NLog.LogManager.GetCurrentClassLogger()
+            else
+                LogsFileFinder.GetLogger("reset-perms", "ResetPerms")
+    
+        let folderManager = FolderManager.GetFolderManager(logger)
+    
+        let filesWithShebang = findFilesWithShebang (folderManager.GetLocalScriptsFolder ())
+        
+        if dryRun then
+            logger.Info "DRY RUN!"
+        
+        for file in filesWithShebang do
+            logger.Info file
 
     rootCommand.SetHandler(handler, dryRunOption)
 
