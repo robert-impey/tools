54bc0918e5ec36879266d5997c521aaa3cf525b6
Robert Impey <robert-impey@users.noreply.github.com>
2024-10-12 16:47:20 +0100

    RS: Rm unneeded code

--- ResetPerms/ResetPerms.Test/Tests.fs
+++ ResetPerms/ResetPerms.Test/Tests.fs
@@ -2,48 +2,6 @@ module Tests
 
 open Xunit
 
-open ResetPerms.Cygwin
-
 [<Fact>]
-let ``Can convert cygwin path`` () =
-    let input = "/cygdrive/c/Users/rober/OneDrive/data/Nightly Cron Job Times.xlsx"
-    let output =  convertCygwinToWindows input
-    
-    Assert.True(output.IsSome)
-    Assert.Equal(@"c:\Users\rober\OneDrive\data\Nightly Cron Job Times.xlsx", output.Value)
-
-let changeDirLine = "rsync: [sender] change_dir \"/cygdrive/z/backup\" failed: Permission denied (13)"
-let failedToOpenLine = "rsync: [sender] send_files failed to open \"/cygdrive/c/Users/rober/OneDrive/config/_Common/Notepad++/config.xml\": Permission denied (13)"
-
-[<Fact>]
-let ``Can parse log line`` () =
-    let changeDirLineOutput = parseLogLine changeDirLine
-
-    Assert.True(changeDirLineOutput.IsSome)
-    let changeDirLineRsyncLogLine = changeDirLineOutput.Value
-    Assert.Equal("sender", changeDirLineRsyncLogLine.Actor.Value)
-    Assert.Equal("change_dir", changeDirLineRsyncLogLine.Message.Value)
-    Assert.Equal("/cygdrive/z/backup", changeDirLineRsyncLogLine.FileName.Value)
-    Assert.Equal("Permission denied (13)", changeDirLineRsyncLogLine.ErrorMessage.Value)
-    
-    let failedToOpenLineOutput = parseLogLine failedToOpenLine
-
-    Assert.True(failedToOpenLineOutput.IsSome)
-    let failedToOpenLineRsyncLogLine = failedToOpenLineOutput.Value
-    Assert.Equal("sender", failedToOpenLineRsyncLogLine.Actor.Value)
-    Assert.Equal("send_files failed to open", failedToOpenLineRsyncLogLine.Message.Value)
-    Assert.Equal("/cygdrive/c/Users/rober/OneDrive/config/_Common/Notepad++/config.xml", failedToOpenLineRsyncLogLine.FileName.Value)
-    Assert.Equal("Permission denied (13)", failedToOpenLineRsyncLogLine.ErrorMessage.Value)
-
-[<Fact>]
-let ``Can extract file name from log line`` () =
-    let changeDirLineOutput = extractFailedFile changeDirLine
-    
-    Assert.True(changeDirLineOutput.IsSome)
-    Assert.Equal("/cygdrive/z/backup", changeDirLineOutput.Value)
-
-    let failedToOpenLineOutput = extractFailedFile failedToOpenLine
-    
-    Assert.True(failedToOpenLineOutput.IsSome)
-    Assert.Equal("/cygdrive/c/Users/rober/OneDrive/config/_Common/Notepad++/config.xml", failedToOpenLineOutput.Value)
-
+let ``Test 1`` () =
+    Assert.True(true)
--- ResetPerms/ResetPerms/Program.fs
+++ ResetPerms/ResetPerms/Program.fs
@@ -1,124 +1,14 @@
 ﻿open System.IO
 open System
-open System.Diagnostics
 open System.CommandLine
-
-open ResetPerms.Cygwin
-
-type LocationFolder = { Location: string; Folder: string }
-
-let findLatestErrFileInfo logsDirectory =
-    let logsDirectoryInfo =
-        System.IO.DirectoryInfo(logsDirectory)
-
-    let errFiles =
-        logsDirectoryInfo.GetFiles("*.err")
-
-    let hasContentFilter =
-        fun (fi: FileInfo) -> fi.Length > 0
-
-    let errFilesWithContent =
-        errFiles |> Array.where (hasContentFilter)
-
-    match errFilesWithContent.Length with
-    | 0 -> None
-    | 1 -> Some errFiles.[0]
-    | _ ->
-        let sortedByDate =
-            errFiles
-            |> Array.sortByDescending (fun (fi: FileInfo) -> fi.LastAccessTimeUtc)
-
-        Some sortedByDate.[0]
-
-let extractFailedFiles (errFile: string) =
-    File.ReadAllLines(errFile)
-    |> Array.choose (extractFailedFile)
-    |> Array.choose (convertCygwinToWindows)
-
-let failedFileToLocationFolder (locations: string list) (failedFile: string) =
-    let rec findLocation (locations': string list) =
-        match locations' with
-        | [] -> None
-        | x :: xs ->
-            if failedFile.StartsWith(x, StringComparison.InvariantCultureIgnoreCase) then
-                Some x
-            else
-                findLocation xs
-
-    let location = findLocation locations
-
-    match location with
-    | Some location' ->
-        let startIndex = location'.Length + 1
-
-        let pathInLocation =
-            failedFile.Substring(startIndex)
-
-        let endIndex = pathInLocation.IndexOf('\\')
-
-        let folder = 
-            if endIndex < 0 then
-                pathInLocation
-            else
-                pathInLocation.Substring(0, endIndex)
-
-        Some({ Location = location'
-               Folder = folder })
-    | None -> None
-
-let locationFolderToBatch (resetScriptDirectory: string) (locationFolder: LocationFolder) =
-
-    let location =
-        locationFolder.Location.Replace('\\', '_')
-
-    let indexOfColon = location.IndexOf(':')
-
-    let locationWithoutColon =
-        if indexOfColon < 0 then
-            location
-        else
-            location.Substring(0, indexOfColon)
-            + location.Substring(indexOfColon + 1)
-
-    let script =
-        locationWithoutColon
-        + "_"
-        + locationFolder.Folder
-        + ".bat"
-
-    Path.Combine(resetScriptDirectory, script)
-
-let resetPermsFolder (dryRun: bool) (locationFolder: LocationFolder) (resetScriptDirectory: string) =
-    let batch = locationFolderToBatch resetScriptDirectory locationFolder
-
-    if (File.Exists(batch)) then
-        printfn "Executing %s" batch
-
-        if dryRun then
-            printfn "DRY RUN (execution skipped)"
-        else
-            let batchProcess = Process.Start(batch)
-            batchProcess.WaitForExit()
-            batchProcess.Close()
-    else
-        printfn "Skipping %s" batch
-
-let searchLogsDirectory (dryRun: bool) (locations: string list) (resetScriptDirectory: string) (errFile: string) =
-    let failedLocationFolders =
-        extractFailedFiles errFile
-        |> Array.choose (failedFileToLocationFolder locations)
-        |> Array.distinct
-
-    for failed in failedLocationFolders do
-        resetPermsFolder dryRun failed resetScriptDirectory
 
 let findDefaultLocationsFile () =
     let name =
         Path.Combine(
-            System.Environment.GetEnvironmentVariable("USERPROFILE"),
+            Environment.GetEnvironmentVariable("USERPROFILE"),
             "Dropbox",
             "local-scripts",
-            System.Environment.GetEnvironmentVariable("ComputerName"),
+            Environment.GetEnvironmentVariable("ComputerName"),
             "reset-perms",
             "locations.txt"
         )
@@ -128,18 +18,6 @@ let findDefaultLocationsFile () =
     else
         None
 
-let readLocationsFile (locationsFile: string) =
-    let f =
-        fun s ->
-            if String.IsNullOrWhiteSpace(s) then
-                None
-            else
-                Some s
-
-    File.ReadAllLines(locationsFile)
-    |> Array.choose (f)
-    |> Array.toList
-
 [<EntryPoint>]
 let main (args) =
     let dryRunOption =
@@ -147,40 +25,18 @@ let main (args) =
 
     let locationsFileOption =
         Option<string option>("--locations-file", findDefaultLocationsFile)
-
-    let resetScriptDirectory =
-        Path.Combine(System.Environment.GetEnvironmentVariable("USERPROFILE"), "autogen", "reset-perms")
-
-    let findDefaultErrFile () =
-        let logsDirectory = 
-            Path.Combine(System.Environment.GetEnvironmentVariable(
-                "USERPROFILE"), "logs", "synch")
-        let latestErrFileInfo = 
-            findLatestErrFileInfo logsDirectory
-        match latestErrFileInfo with
-        | Some latestErrFileInfo' -> Some latestErrFileInfo'.FullName
-        | None -> None
-
-    let errFileOption = Option<string option>("--err-file", findDefaultErrFile)
-
     let rootCommand =
         RootCommand("Reset permissions for files that failed synch")
 
     rootCommand.AddOption(dryRunOption)
     rootCommand.AddOption(locationsFileOption)
-    rootCommand.AddOption(errFileOption)
 
     let handler (dryRun: bool) (locationsFile : string option) (errFile : string option) =
         match locationsFile with
         | None -> printfn "No locations file found in the default location - quitting"
         | Some locationsFile' ->
-            match errFile with
-            | None -> eprintfn "No error file provided or found!"
-            | Some errFile' ->
-                let locations = readLocationsFile locationsFile'
+                printfn $"%b{dryRun} %s{locationsFile'}"
 
-                searchLogsDirectory dryRun locations resetScriptDirectory errFile'
-
-    rootCommand.SetHandler(handler, dryRunOption, locationsFileOption, errFileOption)
+    rootCommand.SetHandler(handler, dryRunOption, locationsFileOption)
 
     rootCommand.Invoke(args)
