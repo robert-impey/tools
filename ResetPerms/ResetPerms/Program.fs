ba1e46e5c97b21289b5008544373a96c032fc907
Robert Impey <robert-impey@users.noreply.github.com>
2024-10-12 16:56:01 +0100

    RP: Running the program with different args

--- ResetPerms/ResetPerms/Program.fs
+++ ResetPerms/ResetPerms/Program.fs
@@ -1,42 +1,18 @@
-﻿open System.IO
-open System
-open System.CommandLine
-
-let findDefaultLocationsFile () =
-    let name =
-        Path.Combine(
-            Environment.GetEnvironmentVariable("USERPROFILE"),
-            "Dropbox",
-            "local-scripts",
-            Environment.GetEnvironmentVariable("ComputerName"),
-            "reset-perms",
-            "locations.txt"
-        )
-
-    if (File.Exists(name)) then
-        Some name
-    else
-        None
+﻿open System.CommandLine
 
 [<EntryPoint>]
 let main (args) =
     let dryRunOption =
         Option<bool>("--dry-run", (fun () -> false))
 
-    let locationsFileOption =
-        Option<string option>("--locations-file", findDefaultLocationsFile)
     let rootCommand =
         RootCommand("Reset permissions for files that failed synch")
 
     rootCommand.AddOption(dryRunOption)
-    rootCommand.AddOption(locationsFileOption)
 
-    let handler (dryRun: bool) (locationsFile : string option) (errFile : string option) =
-        match locationsFile with
-        | None -> printfn "No locations file found in the default location - quitting"
-        | Some locationsFile' ->
-                printfn $"%b{dryRun} %s{locationsFile'}"
+    let handler (dryRun: bool) =
+        printfn $"%b{dryRun}"
 
-    rootCommand.SetHandler(handler, dryRunOption, locationsFileOption)
+    rootCommand.SetHandler(handler, dryRunOption)
 
     rootCommand.Invoke(args)
