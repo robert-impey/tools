230312946482fce01cac0a305909fda66edaf9e7
Robert Impey <robert-impey@users.noreply.github.com>
2024-10-23 19:09:09 +0200

    GWCSS: Logging, CLI and Launch

--- GenerateWindowsConfigSynchScripts/GenerateWindowsConfigSynchScripts/Program.fs
+++ GenerateWindowsConfigSynchScripts/GenerateWindowsConfigSynchScripts/Program.fs
@@ -1,2 +1,33 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System.CommandLine
+open FolderManager
+open Microsoft.Extensions.Logging
+
+[<EntryPoint>]
+let main args =
+    let dryRunOption =
+        Option<bool>("--dry-run", (fun () -> false))
+
+    let rootCommand =
+        RootCommand("Reset permissions for files that have a shebang")
+
+    rootCommand.AddOption(dryRunOption)
+    
+    let handler (dryRun: bool) =
+        let logger =
+            if dryRun then
+                LoggerFactory.Create(fun builder ->
+                    builder.ClearProviders() |> ignore
+                    builder.AddConsole() |> ignore).CreateLogger<FolderManager>()
+            else
+                LogsFileFinder.GetLogger<FolderManager>("synch", "GenerateWindowsConfigSynchScripts")
+    
+        let folderManager = FolderManager.GetFolderManager(logger)
+
+        if dryRun then
+            logger.LogInformation "DRY RUN!"
+        else
+            logger.LogInformation "NOT DRY RUN!"
+
+    rootCommand.SetHandler(handler, dryRunOption)
+
+    rootCommand.Invoke(args)
