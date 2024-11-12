0acf3bf40fc075ec93a5a92488b0abfb88247647
Robert Impey <robert-impey@users.noreply.github.com>
2024-11-12 22:03:57 +0000

    RP: Fmt

--- ResetPerms/ResetPerms/Program.fs
+++ ResetPerms/ResetPerms/Program.fs
@@ -6,51 +6,59 @@ open Microsoft.Extensions.Logging
 
 let fileHasShebang (fileName: string) =
     let firstLine = File.ReadAllLines fileName |> Seq.take 1 |> Seq.toArray
-    
+
     match firstLine.Length with
     | 1 -> firstLine.[0].StartsWith("#!")
     | _ -> false
 
 let findFilesWithShebang (scriptsDir: string) =
-    let matchingFiles = Directory.EnumerateFiles(scriptsDir, "*", SearchOption.AllDirectories)
+    let matchingFiles =
+        Directory.EnumerateFiles(scriptsDir, "*", SearchOption.AllDirectories)
+
     matchingFiles |> Seq.filter fileHasShebang
 
 [<EntryPoint>]
 let main args =
-    let dryRunOption =
-        Option<bool>("--dry-run", (fun () -> false))
+    let dryRunOption = Option<bool>("--dry-run", (fun () -> false))
 
-    let rootCommand =
-        RootCommand("Reset permissions for files that have a shebang")
+    let rootCommand = RootCommand("Reset permissions for files that have a shebang")
 
     rootCommand.AddOption(dryRunOption)
-    
+
     let handler (dryRun: bool) =
         let logger =
             if dryRun then
-                LoggerFactory.Create(fun builder ->
-                    builder.ClearProviders() |> ignore
-                    builder.AddConsole() |> ignore).CreateLogger<FolderManager>()
+                LoggerFactory
+                    .Create(fun builder ->
+                        builder.ClearProviders() |> ignore
+                        builder.AddConsole() |> ignore)
+                    .CreateLogger<FolderManager>()
             else
                 LogsFileFinder.GetLogger<FolderManager>("reset-perms", "ResetPerms")
-    
+
         let folderManager = FolderManager.GetFolderManager(logger)
-    
-        let filesWithShebang = findFilesWithShebang (folderManager.GetLocalScriptsFolder ())
-        
+
+        let filesWithShebang = findFilesWithShebang (folderManager.GetLocalScriptsFolder())
+
         if dryRun then
             logger.LogInformation "DRY RUN!"
-        
+
         logger.LogInformation $"Found {filesWithShebang.Count()} files with shebangs"
-        
+
         for file in filesWithShebang do
             logger.LogInformation $"File with shebang: {file}"
-            
+
             if not dryRun then
-                File.SetUnixFileMode(file,
-                     UnixFileMode.UserRead ||| UnixFileMode.UserWrite ||| UnixFileMode.UserExecute |||
-                     UnixFileMode.GroupRead ||| UnixFileMode.GroupExecute |||
-                     UnixFileMode.OtherRead ||| UnixFileMode.OtherExecute);
+                File.SetUnixFileMode(
+                    file,
+                    UnixFileMode.UserRead
+                    ||| UnixFileMode.UserWrite
+                    ||| UnixFileMode.UserExecute
+                    ||| UnixFileMode.GroupRead
+                    ||| UnixFileMode.GroupExecute
+                    ||| UnixFileMode.OtherRead
+                    ||| UnixFileMode.OtherExecute
+                )
 
     rootCommand.SetHandler(handler, dryRunOption)
 
