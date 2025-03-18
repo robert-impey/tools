75cd3442255a4db4fd2716d7802f678f824a95b8
Robert Impey <robert-impey@users.noreply.github.com>
2025-03-18 19:04:09 +0000

    SD: Test for weird file names

--- staydeleted/sdlib/sdlib_test.go
+++ staydeleted/sdlib/sdlib_test.go
@@ -28,33 +28,36 @@ func TestGetSdFolder(t *testing.T) {
 func TestSetGetAction(t *testing.T) {
 	dir := t.TempDir()
 
-	tfn := "test.txt"
-	tfp := filepath.Join(dir, tfn)
+	tfns := [...]string{"test.txt", "file with spaces.txt", "file with [].txt"}
 
-	tf, _ := os.Create(tfp)
-	defer tf.Close()
+	for _, tfn := range tfns {
+		tfp := filepath.Join(dir, tfn)
 
-	action := Keep
-	err := SetActionForFile(tfp, action)
-	if err != nil {
-		t.Error(err)
-	}
+		tf, _ := os.Create(tfp)
+		defer tf.Close()
 
-	sdfp, err := GetSdFile(tfp)
-	if err != nil {
-		t.Error(err)
-	}
+		action := Keep
+		err := SetActionForFile(tfp, action)
+		if err != nil {
+			t.Error(err)
+		}
 
-	gotAction, err := GetActionForFile(sdfp, dir, os.Stderr)
-	if err != nil {
-		t.Error(err)
-	}
+		sdfp, err := GetSdFile(tfp)
+		if err != nil {
+			t.Error(err)
+		}
 
-	if gotAction.File != tfp {
-		t.Error(fmt.Sprintf("gotAction.File is '%s', expecting '%s'!", gotAction.File, tfp))
-	}
+		gotAction, err := GetActionForFile(sdfp, dir, os.Stderr)
+		if err != nil {
+			t.Error(err)
+		}
 
-	if gotAction.Action != action {
-		t.Error(fmt.Sprintf("gotAction.Action: %s!", getStringForAction(gotAction.Action)))
+		if gotAction.File != tfp {
+			t.Error(fmt.Sprintf("gotAction.File is '%s', expecting '%s'!", gotAction.File, tfp))
+		}
+
+		if gotAction.Action != action {
+			t.Error(fmt.Sprintf("gotAction.Action: %s!", getStringForAction(gotAction.Action)))
+		}
 	}
 }
