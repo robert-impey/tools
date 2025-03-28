b9d70d748c728743c33cade40a98eb1ae31e6303
Robert Impey <robert-impey@users.noreply.github.com>
2025-03-28 22:30:16 +0000

    LMF: Write the contents

--- list_managed_folders/src/main.rs
+++ list_managed_folders/src/main.rs
@@ -66,4 +66,25 @@ fn write_managed_folders_file(
     println!("locations_path: {}", locations_path.display());
     println!("folders_path: {}", folders_path.display());
     println!("Writing to: {}", managed_folders_file_path.display());
+
+    let mut output = String::new();
+
+    if let Some(locations_contents) = fs::read_to_string(locations_path).ok() {
+        if let Some(folders_contents) = fs::read_to_string(folders_path).ok() {
+            for location_line in locations_contents.lines() {
+                for folders_line in folders_contents.lines() {
+                    let managed_folder_path = Path::new(location_line).join(folders_line);
+
+                    if managed_folder_path.exists() {
+                        if let Some(managed_folder_str) = managed_folder_path.to_str() {
+                            output.push_str(managed_folder_str);
+                            output.push_str("\n");                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    let _ = fs::write(managed_folders_file_path, output);
 }
