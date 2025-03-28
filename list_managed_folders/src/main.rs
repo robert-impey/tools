34e370686a8292fb90c58077a2f50b05712f033c
Robert Impey <robert-impey@users.noreply.github.com>
2025-03-28 16:44:00 +0000

    LMF: Printing or writing

--- list_managed_folders/src/main.rs
+++ list_managed_folders/src/main.rs
@@ -1,5 +1,5 @@
-use std::path::PathBuf;
-use clap::{Parser};
+use clap::Parser;
+use std::path::{Path, PathBuf};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -20,20 +20,34 @@ struct Cli {
 fn main() {
     let cli = Cli::parse();
 
-    if let Some(folders_path) = cli.folders.as_deref()
-    {
-        println!("Value for folders_path: {}", folders_path.display());
-
-        if let Some(locations_path) = cli.locations.as_deref()
-        {
-            println!("Value for locations_path: {}", locations_path.display());
-            
+    if let Some(folders_path) = cli.folders.as_deref() {
+        if let Some(locations_path) = cli.locations.as_deref() {
             if cli.write {
-                if let Some(managed_folders_file_path) = cli.managed_folders_file.as_deref()
-                {
-                    println!("Value for managed_folders_file_path: {}", managed_folders_file_path.display());
+                if let Some(managed_folders_file_path) = cli.managed_folders_file.as_deref() {
+                    write_managed_folders_file(
+                        locations_path.into(),
+                        folders_path.into(),
+                        managed_folders_file_path.into(),
+                    );
                 }
+            } else {
+                print_managed_folders(locations_path.into(), folders_path.into());
             }
         }
     }
+}
+
+fn print_managed_folders(locations_path: Box<Path>, folders_path: Box<Path>) {
+    println!("folders_path: {}", folders_path.display());
+    println!("locations_path: {}", locations_path.display());
+}
+
+fn write_managed_folders_file(
+    locations_path: Box<Path>,
+    folders_path: Box<Path>,
+    managed_folders_file_path: Box<Path>,
+) {
+    println!("folders_path: {}", folders_path.display());
+    println!("locations_path: {}", locations_path.display());
+    println!("Writing to: {}", managed_folders_file_path.display());
 }
