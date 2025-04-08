f06f0143152130fb1eefdbbb6b11f785da9b2894
Robert Impey <robert-impey@users.noreply.github.com>
2025-04-08 14:07:28 +0100

    LMF: Mv blank line printing

--- list_managed_folders/src/main.rs
+++ list_managed_folders/src/main.rs
@@ -1,7 +1,7 @@
-use std::fs;
-use clap::Parser;
-use std::path::{Path, PathBuf};
 use chrono::{DateTime, Utc};
+use clap::Parser;
+use std::fs;
+use std::path::{Path, PathBuf};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -26,25 +26,22 @@ fn main() {
         if let Some(locations_path) = cli.locations.as_deref() {
             println!("locations_path: {}", locations_path.display());
             println!("folders_path: {}", folders_path.display());
-            
-            let contents = make_managed_folders_file_contents(
-                locations_path.into(),
-                folders_path.into()
-            );
+
+            let contents =
+                make_managed_folders_file_contents(locations_path.into(), folders_path.into());
 
             if cli.write {
-                if let Some(managed_folders_file_path) 
-                    = cli.managed_folders_file.as_deref() {
+                if let Some(managed_folders_file_path) = cli.managed_folders_file.as_deref() {
                     println!("Writing to: {}", managed_folders_file_path.display());
 
                     let mut file_contents = String::new();
 
                     file_contents.push_str("# AUTOGEN'D!\n# ");
-                
+
                     let now: DateTime<Utc> = Utc::now();
-                
+
                     file_contents.push_str(now.to_rfc2822().as_str());
-                
+
                     file_contents.push_str("\n");
                     file_contents.push_str("# DO NOT EDIT!\n");
 
@@ -61,7 +58,7 @@ fn main() {
 
 fn make_managed_folders_file_contents(
     locations_path: Box<Path>,
-    folders_path: Box<Path>
+    folders_path: Box<Path>,
 ) -> String {
     let mut output = String::new();
 
@@ -69,18 +66,16 @@ fn make_managed_folders_file_contents(
         if let Some(folders_contents) = fs::read_to_string(folders_path).ok() {
             for location_line in locations_contents.lines() {
                 let mut first = true;
-                
+
                 for folders_line in folders_contents.lines() {
-                    if first {
-                        output.push_str("\n");
-                        first = false;
-                    }
-                    
-                    let managed_folder_path = 
-                        Path::new(location_line).join(folders_line);
+                    let managed_folder_path = Path::new(location_line).join(folders_line);
 
                     if managed_folder_path.exists() {
                         if let Some(managed_folder_str) = managed_folder_path.to_str() {
+                            if first {
+                                output.push_str("\n");
+                                first = false;
+                            }
                             output.push_str(managed_folder_str);
                             output.push_str("\n");
                         }
@@ -90,5 +85,5 @@ fn make_managed_folders_file_contents(
         }
     }
 
-    output    
+    output
 }
