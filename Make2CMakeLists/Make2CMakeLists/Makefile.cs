8bb163c3c200cf33b4f40f331c094453c717def7
Robert Impey <robert-impey@users.noreply.github.com>
2025-04-25 14:05:13 +0100

    M2CM: Rm code

--- Make2CMakeLists/Make2CMakeLists/Makefile.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Make2CMakeLists;
-
-internal record Makefile
-{
-    public int CppStandard { get; init; }
-    public required string Src { get; init; }
-    public required string Target { get; init; }
-}
