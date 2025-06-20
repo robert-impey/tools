948d69eaee246b907bbb246c52a26c35165c134a
Robert Impey <robert-impey@users.noreply.github.com>
2025-06-20 20:32:16 +0100

    MF: Notes

--- manfldrs/cmd/root.go
+++ manfldrs/cmd/root.go
@@ -12,14 +12,9 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "managed-folders",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "manfldrs",
+	Short: "A program to manage folders",
+	Long:  `manfldrs is a tool to manage folders on your system.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
