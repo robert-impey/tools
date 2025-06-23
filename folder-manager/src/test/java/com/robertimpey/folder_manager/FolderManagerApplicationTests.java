153ece3464fd253548746e6b83455bd7106449a9
Robert Impey <robert-impey@users.noreply.github.com>
2025-06-23 12:46:20 +0100

    FM: Init of Spring Boot

---
+++ folder-manager/src/test/java/com/robertimpey/folder_manager/FolderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertimpey.folder_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
