1460c118aceac2c78fe39122e64ac4e3f7306735
Robert Impey <robert-impey@users.noreply.github.com>
2025-10-08 11:14:01 +0100

    FM: Rm the CS FM classes

--- FolderManager/FolderManager/LinuxFolderManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.Extensions.Logging;
-
-namespace FolderManager;
-
-public class LinuxFolderManager(ILogger<FolderManager> logger) : UnixFolderManager(logger)
-{
-    protected override string GetLocationsFile() => GetLocationsFile("linux");
-}
--- FolderManager/FolderManager/MacOsFolderManager.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.Extensions.Logging;
-
-namespace FolderManager;
-
-public class MacOsFolderManager(ILogger<FolderManager> logger) : UnixFolderManager(logger)
-{
-    protected override string GetLocationsFile() => GetLocationsFile("darwin");
-}
