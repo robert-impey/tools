8e03fd31afecbcef5ee886c3a952c75150c64039
Robert Impey <robert-impey@users.noreply.github.com>
2025-10-26 16:54:33 +0000

    Rp: Validation on CLI

--- ResetPerms/ResetPerms/CommandSettings.cs
+++ ResetPerms/ResetPerms/CommandSettings.cs
@@ -1,5 +1,6 @@
 using Spectre.Console.Cli;
 using System.ComponentModel;
+using Spectre.Console;
 
 namespace ResetPerms;
 
@@ -7,13 +8,23 @@ public sealed class CommandSettings : Spectre.Console.Cli.CommandSettings
 {
     [CommandOption("-s|--scriptsDirectory")]
     [Description("Path to the scripts directory")]
-    public string ScriptsDirectory { get; set; } = string.Empty;
+    public string? ScriptsDirectory { get; init; }
 
     [CommandOption("--logged")]
     [Description("Logged or not")]
-    public bool Logged { get; set; } = false;
+    [DefaultValue(false)]
+    public bool Logged { get; init; }
 
     [CommandOption("-l|--logsDirectory")]
     [Description("Path to the logs directory")]
-    public string? LogsDirectory { get; set; } = null;
+    public string? LogsDirectory { get; init; }
+    
+    public override ValidationResult Validate()
+    {
+        if (string.IsNullOrWhiteSpace(ScriptsDirectory))
+        {
+            return ValidationResult.Error("The --scriptsDirectory path must be provided.");
+        }
+        return base.Validate();
+    }
 }
