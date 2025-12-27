5d55307c3a67903679ce44c722862f97ae2b5100
Robert Impey <robert-impey@users.noreply.github.com>
2025-12-27 16:41:48 +0000

    Rm unused cobra/viper scaffolding

--- cron-setter/cmd/root.go
+++ cron-setter/cmd/root.go
@@ -35,8 +35,4 @@ func init() {
 	// will be global for your application.
 
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cron-setter.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- gensyn/cmd/root.go
+++ gensyn/cmd/root.go
@@ -40,8 +40,4 @@ func init() {
 	// will be global for your application.
 
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tools.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
