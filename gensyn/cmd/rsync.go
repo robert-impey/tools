47df26d57dc53b0fba4421c1ca71b1849442ccb4
Robert Impey <robert-impey@users.noreply.github.com>
2026-01-28 13:46:45 +0000

    GSS: Update comments

--- gensyn/cmd/rsync.go
+++ gensyn/cmd/rsync.go
@@ -17,13 +17,9 @@ import (
 // rsyncCmd represents the rsync command
 var rsyncCmd = &cobra.Command{
 	Use:   "rsync",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A program for generating scripts for synchronising directories using rsync",
+	Long: `A program for generating scripts for synchronising directories using rsync
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		rsync(args)
 	},
