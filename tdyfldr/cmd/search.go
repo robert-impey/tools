e6e6ea51e4e4178b1779266bb66abcdc40db645c
Robert Impey <robert-impey@users.noreply.github.com>
2026-03-29 16:16:42 +0100

    TF: Ported the search subcommand

---
+++ tdyfldr/cmd/search.go
@@ -0,0 +1,27 @@
+﻿package tdyfldr
+
+import (
+	"github.com/robert-impey/tools/tdyfldr/internal"
+	"github.com/spf13/cobra"
+)
+
+var searchLogsDir string
+
+var searchCmd = &cobra.Command{
+	Use:   "search <directory>",
+	Short: "Search a single directory",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var logsDirPtr *string
+		if searchLogsDir != "" {
+			logsDirPtr = &searchLogsDir
+		}
+
+		return internal.SearchDirectory(args[0], logsDirPtr)
+	},
+}
+
+func init() {
+	searchCmd.Flags().StringVar(&searchLogsDir, "logs-dir", "", "Directory where logs should be written")
+	rootCmd.AddCommand(searchCmd)
+}
