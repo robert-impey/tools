948f8a6510237372a2445aaee599383afb482839
Robert Impey <robert-impey@users.noreply.github.com>
2026-07-12 14:45:58 +0100

    Added test for clean file name

---
+++ internal/script_gen_test.go
@@ -0,0 +1,23 @@
+package internal
+
+import (
+	"testing"
+)
+
+func TestCleanFolderPathForLogName(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"/path/to/folder", "path_to_folder"},
+		{"C:\\path\\to\\folder", "C_path_to_folder"},
+		{"user@host:/path/to/folder", "user__host__path_to_folder"},
+	}
+
+	for _, test := range tests {
+		result := CleanFolderPathForLogName(test.input)
+		if result != test.expected {
+			t.Errorf("CleanFolderPathForLogName(%q) = %q; want %q", test.input, result, test.expected)
+		}
+	}
+}
