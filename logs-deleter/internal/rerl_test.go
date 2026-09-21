4358666859bf80e045bb63bb6e32ebce374547bd
Robert Impey <robert-impey@users.noreply.github.com>
2026-09-21 22:20:12 +0100

    LD: Robocopy: Read test files rather than temps

--- logs-deleter/internal/rerl_test.go
+++ logs-deleter/internal/rerl_test.go
@@ -1,7 +1,6 @@
 ﻿package internal
 
 import (
-	"os"
 	"path/filepath"
 	"testing"
 )
@@ -29,15 +28,10 @@ func TestIsFilesCopiedLine_FilesCopied(t *testing.T) {
 func TestFileHasCopies_WhenCopiesExist(t *testing.T) {
 	t.Parallel()
 
-	dir := t.TempDir()
-	filePath := filepath.Join(dir, "with-copies.robocopy-synch.log")
-
-	content := "some header\n    Files :       5117          123       5117          0          0          0\nfooter\n"
-	if err := os.WriteFile(filePath, []byte(content), 0o600); err != nil {
-		t.Fatalf("write file: %v", err)
-	}
+	filePath := filepath.Join("test_data", "robocopy", "copies.log")
 
 	hasCopies, err := FileHasCopies(filePath)
+
 	if err != nil {
 		t.Fatalf("FileHasCopies error: %v", err)
 	}
@@ -50,15 +44,9 @@ func TestFileHasCopies_WhenCopiesExist(t *testing.T) {
 func TestFileHasCopies_WhenNoCopiesExist(t *testing.T) {
 	t.Parallel()
 
-	dir := t.TempDir()
-	filePath := filepath.Join(dir, "no-copies.robocopy-synch.log")
-
-	content := "some header\n    Files :       5117          0       5117          0          0          0\nfooter\n"
-	if err := os.WriteFile(filePath, []byte(content), 0o600); err != nil {
-		t.Fatalf("write file: %v", err)
-	}
-
+	filePath := filepath.Join("test_data", "robocopy", "empty.log")
 	hasCopies, err := FileHasCopies(filePath)
+
 	if err != nil {
 		t.Fatalf("FileHasCopies error: %v", err)
 	}
